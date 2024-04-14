df20e8b34c3987e2b1a12355409fcc35b8b64319
mirrahat <mirrahatfiverr@gmail.com>
2024-04-14 16:23:45 +0600

    databasse created

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base (options)
+        {
+                
+        }
+    }
+}
