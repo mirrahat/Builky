33fc1002f594eceef3b06b4cad694f5fe23fa9e3
mirrahat <mirrahatfiverr@gmail.com>
2024-04-20 20:04:47 +0600

    Display order done

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController1 : Controller
+    public class CategoryController : Controller
     {
+
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db) {  _db = db; }
         public IActionResult Index()
         {
-            return View();
+
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
         }
     }
 }
