e86cdb5297140c3b317ce04e3af294827c91c8ac
mirrahat <mirrahatfiverr@gmail.com>
2024-04-27 10:54:54 +0600

    validation added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,6 +25,9 @@ namespace BulkyWeb.Controllers
 
         [HttpPost]
         public IActionResult Create(Category category) {
+            if (category.Name == category.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the Name");
+            }
             if (ModelState.IsValid) {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
