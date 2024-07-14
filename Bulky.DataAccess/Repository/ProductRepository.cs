73b15c6d7a012b6ff846b185c8614e9a3cc00969
mirrahat <mirrahatfiverr@gmail.com>
2024-07-15 00:35:21 +0600

    porduct repo created

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+  
+        public class ProductRepository : Repository<Product>, IProductRepository
+        {
+            private ApplicationDBContext _db;
+            public ProductRepository(ApplicationDBContext db) : base(db)
+            {
+                _db = db;
+            }
+            public void Save()
+            {
+                _db.SaveChanges();
+            }
+
+            public void Update(Product entity)
+            {
+                _db.Products.Update(entity);
+            }
+
+           
+        
+    }
+}
