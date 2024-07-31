d60a1a8612d9c74f46eafdd692ab93bce79f9f69
mirrahat <mirrahatfiverr@gmail.com>
2024-07-31 11:40:25 +0600

    update in identity framework

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -22,6 +22,8 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder moedlBuilder) {
 
             base.OnModelCreating(moedlBuilder);
